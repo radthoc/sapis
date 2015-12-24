e55bef26118bc8116f3552f90241276e745c1caa
radtDrumulator <radthoc@gmail.com>
2015-12-24 13:17:01 +0100

    cs checkup

--- tests/AppBundle/Repository/ItemsRepositoryTest.php
+++ tests/AppBundle/Repository/ItemsRepositoryTest.php
@@ -23,34 +23,40 @@ class ItemsRepositoryTest extends \PHPUnit_Framework_TestCase
             ->andReturn(
                 [
                     [
-                    'id' => 10,
-                    'name' => 'item 1',
-                    'description' => 'lorem ipsum...',
-                    'price' => 8.25
+                        'id' => 10,
+                        'name' => 'item 1',
+                        'description' => 'lorem ipsum...',
+                        'price' => 8.25
                     ],
                     [
-                    'id' => 20,
-                    'name' => 'item 2',
-                    'description' => 'lorem ipsum...',
-                    'price' => 12.59
+                        'id' => 20,
+                        'name' => 'item 2',
+                        'description' => 'lorem ipsum...',
+                        'price' => 12.59
                     ],
                     [
-                    'id' => 10,
-                    'name' => 'item 3',
-                    'description' => 'lorem ipsum...',
-                    'price' => 10.90
+                        'id' => 10,
+                        'name' => 'item 3',
+                        'description' => 'lorem ipsum...',
+                        'price' => 10.90
                     ],
                 ]
             );
-
-        $expected = ['[{"id":10,"name":"item 1","description":"lorem ipsum...","price":8.25},{"id":20,"name":"item 2","description":"lorem ipsum...","price":12.59},{"id":30,"name":"item 3","description":"lorem ipsum...","price":10.9}]','application/json'];
+        
+        $expected = [
+            '[{"id":10,"name":"item 1","description":"lorem ipsum...",' .
+            '"price":8.25},{"id":20,"name":"item2","description":"lorem ipsum...",' .
+            '"price":12.59},{"id":30,"name":"item 3","description":"lorem ipsum...","price":10.9}]',
+            'application/json'
+        ];
 
         $result = $this->itemsRepository->find();
 
-        try {
+        try 
+        {
             $this->assertEquals(serialize($expected), serialize($result));
-        } 
-        catch (PHPUnit_Framework_ExpectationFailedException $e) {
+        } catch (PHPUnit_Framework_ExpectationFailedException $e) 
+        {
             echo $e->getComparisonFailure()->toString();
         }
     }
@@ -68,18 +74,18 @@ class ItemsRepositoryTest extends \PHPUnit_Framework_TestCase
                 ]
             )
             ->andReturn(40);
-        
+
         $item = [
             'name' => 'item 4',
             'description' => 'lorem ipsum... ',
             'price' => 23.60
         ];
-        
+
         $result = $this->itemsRepository->persist($item);
-        
+
         $this->assertEquals(40, $result);
     }
-    
+
     public function testSaveItemsWithId()
     {
         $this->DBHandler->shouldReceive('persist')
@@ -95,15 +101,15 @@ class ItemsRepositoryTest extends \PHPUnit_Framework_TestCase
             )
             ->andReturn(true);
 
-            $item = [
-                'name' => 'item 3',
-                'description' => 'lorem ipsum... ',
-                'price' => 11.41,
-                'id' => 30
-            ];
-            
+        $item = [
+            'name' => 'item 3',
+            'description' => 'lorem ipsum... ',
+            'price' => 11.41,
+            'id' => 30
+        ];
+
         $result = $this->itemsRepository->persist($item);
-        
+
         $this->assertTrue($result);
     }
 }
